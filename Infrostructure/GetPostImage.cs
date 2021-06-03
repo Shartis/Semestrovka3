006ffc46b9b26b96de0001a0c16abde0b4e82cd2
Shartis <salmina.vladlena@mail.ru>
2021-06-03 19:16:14 +0300

    some models and services

---
+++ Infrostructure/GetPostImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semestrovka4.Infrostructure
+{
+    public class GetPostImage
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public GetPostImage(ApplicationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public byte[] GetImage(int postId)
+        {
+            var post = _dbContext.Posts.FirstOrDefault(p => p.Id == postId);
+            if (post == null)
+                return null;
+            var data = post.Image;
+            if (data == null)
+                return null;
+            return data;
+        }
+    }
+}
---
+++ Models/ArticleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semestrovka4.Models
+{
+    public class ArticleViewModel
+    {
+    }
+}
