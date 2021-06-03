20c4e7093b78b2db6c21d4a9deaa61aa09324e93
Shartis <salmina.vladlena@mail.ru>
2021-06-03 19:52:39 +0300

    try

--- Models/Expired.cs
+++ Models/Expired.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace Semestrovka4.Models
 {
     public class Expired
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProfessorName { get; set; }
+        public string Description { get; set; }
     }
 }
+////////
